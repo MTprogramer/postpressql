41edd3e1026feee63f28d09bf8fcb92cb244fd38
mian <programerx55@gmail.com>
2024-09-25 19:53:48 +0500

    dp connected with railway

---
+++ src/main/kotlin/info.kt
@@ -0,0 +1,19 @@
+/**
+ * local db data
+ */
+JDBC_DATABASE_URL=jdbc:postgresql://localhost:5432/test?user=postgres&password=1155
+JDBC_DRIVER=org.postgresql.Driver
+JWT_SECRET=122134435665788790090987
+SECRET_KEY=122134435665788790090987
+
+
+        /**
+ * railway db data
+ */
+JDBC_DATABASE_URL=jdbc:postgresql://junction.proxy.rlwy.net:12684/railway?user=postgres&password=fzcGisRIhMSNZjgePlEGeYbArmaqhTXT
+JDBC_DRIVER=org.postgresql.Driver
+JWT_SECRET=122134435665788790090987
+SECRET_KEY=122134435665788790090987
+
+
+
