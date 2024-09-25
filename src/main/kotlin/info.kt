747bdaf622824c8804a54a633f7926be30f3a36a
mian <programerx55@gmail.com>
2024-09-25 22:52:49 +0500

    dp

--- src/main/kotlin/info.kt
+++ src/main/kotlin/info.kt
@@ -7,7 +7,7 @@ JWT_SECRET=122134435665788790090987
 SECRET_KEY=122134435665788790090987
 
 
-        /**
+/**
  * railway db data
  */
 JDBC_DATABASE_URL=jdbc:postgresql://junction.proxy.rlwy.net:12684/railway?user=postgres&password=fzcGisRIhMSNZjgePlEGeYbArmaqhTXT
